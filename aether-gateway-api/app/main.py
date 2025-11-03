1c2f4c0ef2fadc3e3e91a6f3143f23922edb8cdc
Arunava-27062003 <arunava.kundu@iemlabs>
2025-11-03 17:07:21 +0530

    removed backend, start new inside docker

--- aether-gateway-api/app/main.py
+++
@@ -1,9 +0,0 @@
-# app/main.py
-from fastapi import FastAPI
-from app.models.base import create_db_and_tables
-
-app = FastAPI()
-
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
